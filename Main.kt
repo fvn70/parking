2fc1b3680ede1b4df5fc177ce531c93269c88f20
fvn70 <fvn_reg@mail.ru>
2022-01-17 19:52:45 +0700

    Parking Lot. Stage 3/5: Expand and park

--- Main.kt
+++ Main.kt
@@ -1,13 +1,15 @@
 package parking
 
 fun main() {
-    val p = Parking(2)
-    p.spots[0].free = false
+    val p = Parking(20)
 
-    val cmd = readLine()!!.split(' ')
-    when (cmd[0]) {
-        "park" -> p.park(cmd[1], cmd[2])
-        "leave" -> p.leave(cmd[1].toInt())
+    while (true) {
+        val cmd = readLine()!!.split(' ')
+        when (cmd[0]) {
+            "park" -> p.park(cmd[1], cmd[2])
+            "leave" -> p.leave(cmd[1].toInt())
+            "exit" -> break
+        }
     }
 }
 
@@ -15,13 +17,18 @@ class Parking(val num: Int) {
     val spots = List(num) { Spot() }
 
     fun park(id: String, color: String) {
+        var isFull = true
         for (i in spots.indices) {
             val s = spots[i]
             if (s.isFree()) {
                 s.park(id, color)
                 println("${s.carColor} car parked in spot ${i + 1}.")
+                isFull = false
                 break
             }
+        }
+        if (isFull) {
+            println("Sorry, the parking lot is full.")
         }
     }
 
