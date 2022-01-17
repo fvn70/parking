7d1c6dd005e26a0b53125cd5c7eceab630c90a76
fvn70 <fvn_reg@mail.ru>
2022-01-17 18:57:46 +0700

    Parking Lot. Stage 2/5: Only 2 spots

--- Main.kt
+++ Main.kt
@@ -1,116 +1,55 @@
 package parking
 
 fun main() {
-    var spots = emptyList<Spot>()
-    var isExit = false
-    var isCreate = false
+    val p = Parking(2)
+    p.spots[0].free = false
 
-    while (!isExit) {
-        val cmd = readLine()!!.split(' ')
-        val list = listOf("create", "exit")
+    val cmd = readLine()!!.split(' ')
+    when (cmd[0]) {
+        "park" -> p.park(cmd[1], cmd[2])
+        "leave" -> p.leave(cmd[1].toInt())
+    }
+}
 
-        if (cmd[0] !in list && !isCreate) {
-            println("Sorry, a parking lot has not been created.")
-            continue
-        }
+class Parking(val num: Int) {
+    val spots = List(num) { Spot() }
 
-        when (cmd[0]) {
-            "create" -> {
-                val num = cmd[1].toInt()
-                spots = create(num)
-                isCreate = true
-                println("Created a parking lot with $num spots.")
+    fun park(id: String, color: String) {
+        for (i in spots.indices) {
+            val s = spots[i]
+            if (s.isFree()) {
+                s.park(id, color)
+                println("${s.carColor} car parked in spot ${i + 1}.")
+                break
             }
-            "park" -> parkCar(spots, cmd[1], cmd[2])
-            "leave" -> leaveSpot(spots, cmd[1].toInt())
-            "reg_by_color" -> findByColor(spots, cmd[1], "reg")
-            "spot_by_color" -> findByColor(spots, cmd[1], "spot")
-            "spot_by_reg" -> spotByReg(spots, cmd[1])
-            "status" -> status(spots)
-            "exit" -> isExit = true
+        }
+    }
+
+    fun leave(k: Int) {
+        if (spots[k - 1].isFree()) {
+            println("There is no car in spot $k.")
+        } else {
+            spots[k - 1].leave()
+            println("Spot $k is free.")
         }
     }
 }
 
 class Spot(var free: Boolean = true,
-           var carNum: String = "",
+           var carId: String = "",
            var carColor: String = "") {
-    var spotNum: Int = 0
-    fun park(carNum: String, carColor: String) {
-        this.carNum = carNum
-        this.carColor = carColor
+
+    fun park(id: String, color: String) {
+        carId = id
+        carColor = color
         free = false
     }
 
     fun leave() {
-        carNum = ""
+        carId = ""
         carColor = ""
         free = true
     }
 
     fun isFree() = free
-}
-
-fun create(n: Int): List<Spot> {
-    return List(n) { Spot() }
-}
-
-fun status(park: List<Spot>) {
-    var cnt = 0
-    for (s in park) {
-        if (!s.isFree()) {
-            println("${s.spotNum} ${s.carNum} ${s.carColor}")
-            cnt++
-        }
-    }
-    if (cnt == 0) {
-        println("Parking lot is empty.")
-    }
-}
-
-fun getFreeSpotNum(park: List<Spot>): Int {
-    for (s in park) {
-        if (s.isFree()) return park.indexOf(s) + 1
-    }
-    return 0
-}
-
-fun findByColor(park: List<Spot>, color: String, fld: String) {
-    val list = park.filter { it.carColor.lowercase() == color.lowercase() }
-    if (list.isEmpty()) {
-        println("No cars with color $color were found.")
-    } else {
-        val out = if (fld == "reg") list.map { it.carNum } else list.map { it.spotNum }
-        println(out.joinToString())
-    }
-}
-
-fun spotByReg(park: List<Spot>, num: String) {
-    val list = park.filter { it.carNum.lowercase() == num.lowercase() }
-    if (list.isEmpty()) {
-        println("No cars with registration number $num were found.")
-    } else {
-        println(list.map { it.spotNum }.joinToString())
-    }
-}
-
-fun leaveSpot(park: List<Spot>, n: Int)  {
-    if (park[n - 1].isFree()) {
-        println("There is no car in spot $n.")
-    } else {
-        park[n - 1].leave()
-        println("Spot $n is free.")
-    }
-}
-
-fun parkCar(park: List<Spot>, reg: String, color: String) {
-    val num = getFreeSpotNum(park)
-    if (num > 0) {
-        val s = park[num - 1]
-        s.park(reg, color)
-        s.spotNum = num
-        println("${s.carColor} car parked in spot $num.")
-    } else {
-        println("Sorry, the parking lot is full.")
-    }
 }
